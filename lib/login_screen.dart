9a55a92338bc46f889f91744b36401f959dc291d
Riun <rionsen157@gmail.com>
2025-05-09 18:33:53 +0700

    add frontend login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -41,8 +41,7 @@ class _LoginScreenState extends State<LoginScreen> {
         return;
       }
 
-      // Cek verifikasi email
-      await user.reload(); // refresh user data
+      await user.reload();
       if (!user.emailVerified) {
         await user.sendEmailVerification();
 
@@ -61,7 +60,6 @@ class _LoginScreenState extends State<LoginScreen> {
         return;
       }
 
-      // Ambil role dari Firestore
       final doc =
           await FirebaseFirestore.instance
               .collection('users')
@@ -118,63 +116,184 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: "Email"),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
-                suffixIcon: GestureDetector(
-                  onLongPress: () {
-                    setState(() {
-                      isPasswordVisible = true;
-                    });
-                  },
-                  onLongPressUp: () {
-                    setState(() {
-                      isPasswordVisible = false;
-                    });
-                  },
-                  child: Icon(
-                    isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                  ),
-                ),
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: -28,
+            child: Container(
+              width: 300,
+              height: 300,
+              child: Image.asset(
+                'assets/images/rectangle_2.png',
+                fit: BoxFit.cover,
               ),
-              obscureText: !isPasswordVisible,
             ),
-            if (errorMessage != null) ...[
-              SizedBox(height: 10),
-              Text(errorMessage!, style: TextStyle(color: Colors.red)),
-            ],
-            SizedBox(height: 20),
-            isLoading
-                ? CircularProgressIndicator()
-                : ElevatedButton(onPressed: login, child: Text("Login")),
-            SizedBox(height: 20),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChangePasswordScreen(),
+          ),
+
+          Positioned(
+            top: 0,
+            left: -10,
+            child: Container(
+              width: 215,
+              height: 215,
+              child: Image.asset(
+                'assets/images/rectangle_10.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16, 250, 16, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: 'Fundra',
+                      style: TextStyle(
+                        color: Color(0xFF0E5C36),
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'IN',
+                          style: TextStyle(
+                            color: Color(0xFFE67D13),
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  );
-                },
-                child: Text("Ganti Password?"),
+                  ),
+                  SizedBox(height: 40),
+
+                  TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: Colors.black,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(Icons.email, color: Color(0xFF0E5C36)),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                          color: Color(0xFF0E5C36),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+
+                  TextField(
+                    controller: passwordController,
+                    obscureText: !isPasswordVisible,
+                    cursorColor: Colors.black,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(Icons.lock, color: Color(0xFF0E5C36)),
+
+                      suffixIcon: GestureDetector(
+                        onLongPress: () {
+                          setState(() {
+                            isPasswordVisible = true;
+                          });
+                        },
+                        onLongPressUp: () {
+                          setState(() {
+                            isPasswordVisible = false;
+                          });
+                        },
+                        child: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.black,
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                          color: Color(0xFF0E5C36),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChangePasswordScreen(),
+                          ),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                      ),
+                      child: Text("Forgot Password?"),
+                    ),
+                  ),
+
+                  if (errorMessage != null) ...[
+                    SizedBox(height: 10),
+                    Text(errorMessage!, style: TextStyle(color: Colors.red)),
+                  ],
+
+                  SizedBox(height: 20),
+
+                  isLoading
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                        onPressed: login,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF0E5C36),
+                          foregroundColor: Colors.white,
+                          minimumSize: Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
